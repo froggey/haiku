ac5fb4d73adfad5590308e319dc57d4a7aa4aefc
Henry Harrington <henry.harrington@gmail.com>
2014-10-21 20:14:45 +0100

    Preliminary EFI support in the bootloader.
    Enough to let the kernel to print hello, but not much beyond that.
    I think this also breaks non-EFI x86-64 support, code cleanups required.

--- headers/private/kernel/boot/platform/efi/platform_kernel_args.h
+++ headers/private/kernel/boot/platform/efi/platform_kernel_args.h
@@ -5,7 +5,10 @@
 #	error This file is included from <boot/kernel_args.h> only
 #endif
 
-#warning EFI: IMPLEMENT platform_kernel_args.h
+// currently the EFI loader pretends to be the bios_ia32 platform.
+// not quite right, as the kernel needs to be aware of efi runtime services
+
+#include <arch/x86/apm.h>
 
 #define SMP_MAX_CPUS 64
 
@@ -15,13 +18,20 @@
 
 #define MAX_SERIAL_PORTS 4
 
+typedef struct bios_drive {
+	struct bios_drive	*next;
+	uint16				drive_number;
+	disk_identifier		identifier;
+} bios_drive;
+
 typedef struct {
 	uint16		serial_base_ports[MAX_SERIAL_PORTS];
 
-//	FixedWidthPointer<bios_drive> drives;
+	FixedWidthPointer<bios_drive> drives;
 		// this does not contain the boot drive
+	// seems to be ignored entirely?
 
-//	apm_info	apm;
+	apm_info	apm;
 } _PACKED platform_kernel_args;
 
 #endif	/* KERNEL_BOOT_PLATFORM_BIOS_IA32_KERNEL_ARGS_H */
